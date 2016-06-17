3fa7f246a4f799cd7c1c3ce7ce6eae88295aea02
Will Dixon <dixonwille@hotmail.com>
2016-06-17 11:22:38 -0400

    Implemented a basic Avatar given initials

--- avatar.go
+++ avatar.go
@@ -1,33 +1,64 @@
 package avatar
 
 import (
+	"fmt"
 	"io"
+	"os"
 
 	"github.com/ajstarks/svgo"
 )
 
 //Avatar is used to create the svg
 type Avatar struct {
-	SVG      *svg.SVG
-	Initials string
-	Colors   []Color
+	svg      *svg.SVG
+	initials string
+	color    Color
+	size     int
+	title    string
+	desc     string
 }
+
+const (
+	size = 200
+	desc = "This Avatar was created using InitialAvatars from https://github.com/dixonwille/InitialAvatars"
+)
 
 //NewAvatar populates the Avatar stucture so that you can create the Avatar.
 //Does not populate the SVG field until we know what we are writting to.
-func NewAvatar(initials string, colors ...Color) *Avatar {
+func NewAvatar(initials string, color Color) *Avatar {
+	var title = "Avatar for " + initials
 	return &Avatar{
-		Initials: initials,
-		Colors:   colors,
+		initials: initials,
+		color:    color,
+		size:     size,
+		title:    title,
+		desc:     desc,
 	}
 }
 
 //CreateFile writes the Avatar to the file specified.
-func (a *Avatar) CreateFile(filename string) {
-
+func (a *Avatar) CreateFile(filename string) error {
+	f, err := os.Create(filename)
+	if err != nil {
+		return err
+	}
+	defer f.Close()
+	a.svg = svg.New(f)
+	a.makeSVG()
+	return nil
 }
 
 //Create writes the Avatar svg to the io.writer.
 func (a *Avatar) Create(w io.Writer) {
-	a.SVG = svg.New(w)
+	a.svg = svg.New(w)
+	a.makeSVG()
+}
+
+func (a *Avatar) makeSVG() {
+	a.svg.Startview(a.size, a.size, 0, 0, a.size, a.size)
+	a.svg.Title(a.title)
+	a.svg.Desc(a.desc)
+	a.svg.Circle(a.size/2, a.size/2, a.size/2, fmt.Sprintf("fill:%s", a.color.String()))
+	a.svg.Text(a.size/2, a.size/2, a.initials, fmt.Sprintf("font-family:Tahoma, Geneva, sans-serif;text-anchor:middle;alignment-baseline:central;font-size:%fpx;fill:white", float64(a.size)*0.50))
+	a.svg.End()
 }
--- colors.go
+++ colors.go
@@ -1,5 +1,10 @@
 package avatar
 
+import (
+	"encoding/hex"
+	"fmt"
+)
+
 //Color is the RGBA value of any color.
 type Color struct {
 	R int
@@ -8,10 +13,14 @@ type Color struct {
 	A float64
 }
 
+const (
+	rgbaFormater = "rgba(%d, %d, %d, %f)"
+)
+
 //NewColor creates a color with the given Red, Green, Blue, and Alpha values.
 //Red, Green, and Blue are integers with values from 0 to 255.
 //Alpha is measure from 0.0 (transparent) to 1.0 (opaque).
-func newColor(r, g, b int, a float64) (*Color, error) {
+func NewColor(r, g, b int, a float64) (*Color, error) {
 	if r < 0 || r > 255 {
 		return nil, newError(errValueOutOfRange, "Red")
 	}
@@ -32,8 +41,24 @@ func newColor(r, g, b int, a float64) (*Color, error) {
 	}, nil
 }
 
-//ChangeAlpha changes the alpha channel to specified value.
-//Alpha is measure from 0.0 (transparent) to 1.0 (opaque).
-func (c *Color) changeAlpha(a float64) {
-	c.A = a
+//ColorFromHex creates the color from hex value.
+//The Alpha channel will be 1.0.
+func ColorFromHex(hexString string) (*Color, error) {
+	if len(hexString) > 6 {
+		return nil, newError(errValueOutOfRange, "Hex")
+	}
+	hexBytes, err := hex.DecodeString(hexString)
+	if err != nil {
+		return nil, err
+	}
+	return &Color{
+		R: int(hexBytes[0]),
+		G: int(hexBytes[1]),
+		B: int(hexBytes[2]),
+		A: 1.0,
+	}, nil
+}
+
+func (c *Color) String() string {
+	return fmt.Sprintf(rgbaFormater, c.R, c.G, c.B, c.A)
 }
